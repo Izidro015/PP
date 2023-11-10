86561408e9974cc6ac607af201e242f58b1f8c6d
Matheus Izidro C dos Santos <127612958+Izidro015@users.noreply.github.com>
2023-11-10 16:15:09 -0300

    Delete 1E02.c

--- 1E02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float x;
-    printf("digite um numero real:");
-    scanf("%f", &x); //& e o terminal doq colocar no x
-    printf("voce digitou o numero: %f", x);
-    return 0;
-}
